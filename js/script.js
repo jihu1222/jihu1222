5850dd54fdae7c7f4f7b868f867c52c6edf1d933
Kai Hughes <70239072+kaihues@users.noreply.github.com>
2022-03-31 16:31:19 -0600

    added button name change and hover image

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
-const para = document.querySelector('p');
+const button = document.getElementById('a11');
 
-para.addEventListener('click', updateName);
+button.addEventListener('click', updateName);
+
+const image = document.getElementById('coolImage');
+
+image.addEventListener('mouseover', updateImg);
 
 function updateName () {
   const name = prompt('Enter a new name');
-  para.textContent = `Player 1: ${name}`
+  button.textContent = `${name}!`
+}
+
+function updateImg () {
+  image.src = "../img/cadt-angy.png"
 }
