0630b620cc8dd00aee2b59bdcf6134e7f34f8313
Kai Hughes <70239072+kaihues@users.noreply.github.com>
2022-04-30 20:47:50 -0600

    added music note credits

--- js/music.js
+++ js/music.js
@@ -3,6 +3,7 @@
 const image1 = document.getElementById('musiCat1');
 image1.addEventListener('click', meow1);
 var sound1 = new Audio("./sounds/cNote.wav");
+// audio is from here: https://freesound.org/people/Carlos_Vaquero/sounds/154111/
 
 function meow1 () {
   image1.src = "./img/cadt-angy.PNG";
@@ -19,6 +20,7 @@ function returnImg1 () {
 const image2 = document.getElementById('musiCat2');
 image2.addEventListener('click', meow2);
 var sound2 = new Audio("./sounds/dNote.wav");
+// audio is from here: https://freesound.org/people/Carlos_Vaquero/sounds/154113/
 
 function meow2 () {
   image2.src = "./img/cadt-angy.PNG";
@@ -35,6 +37,7 @@ function returnImg2 () {
 const image3 = document.getElementById('musiCat3');
 image3.addEventListener('click', meow3);
 var sound3 = new Audio("./sounds/fNote.mp3");
+// audio is from here: https://freesound.org/people/Carlos_Vaquero/sounds/154116/
 
 function meow3 () {
   image3.src = "./img/cadt-angy.PNG";
